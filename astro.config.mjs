a0a6be1a5902e5542c5e64e8738cb9ccf046c8fc
Matias Contreras <matiasleoncontreras@gmail.com>
2025-10-20 19:29:31 -0300

    add adapter-cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
+import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
+  output: 'server',
   integrations: [react()],
-
+  // @ts-ignore
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        'react-dom/server': 'react-dom/server.edge', // 💡 Alias para evitar CommonJS
+      }
+    }
+  },
 });
