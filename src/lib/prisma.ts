2b0fe2ada5976ac03cbc5664b8a21a57822a2f1b
Matias Contreras <matiasleoncontreras@gmail.com>
2026-01-23 23:00:58 -0300

    delete prisma/client/edge

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client/edge"
+// import { PrismaClient } from "@prisma/client/edge" // Required for Cloudflare/Edge deployment
+import { PrismaClient } from "@prisma/client"
 import { withAccelerate } from "@prisma/extension-accelerate"
 
 export const getDb = (databaseUrl: string) => {
